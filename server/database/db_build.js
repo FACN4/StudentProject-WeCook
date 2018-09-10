1516c2647b0bb4b10b85e289d5c1a8c227f6434f
DenisKent <kentdenis6@hotmail.co.uk>
2018-09-10 14:31:39 +0300

    renamed dummy_data to cook_filler_data

--- server/database/db_build.js
+++ server/database/db_build.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const { db } = require('./db_connection');
 
 const emptyTables = fs.readFileSync(`${__dirname}/build_tables.sql`, 'utf-8');
-const dummyData = fs.readFileSync(`${__dirname}/db_dummy_data.sql`, 'utf-8');
+const dummyData = fs.readFileSync(`${__dirname}/cook_filler_data.sql`, 'utf-8');
 
 if (process.argv[2] === 'run') {
   db.none(emptyTables)
