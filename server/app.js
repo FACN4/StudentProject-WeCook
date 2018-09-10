e9f1081dc34562f9877f248cc1be51f8b3cc33eb
afifisuha <afifisuha@campus.technion.ac.il>
2018-09-10 10:47:18 +0300

    updated file structure #4

--- server/app.js
+++ server/app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const controllers = require('./controllers/index.js');
 
 const app = express();
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5000);
 app.use(controllers);
 module.exports = app;
