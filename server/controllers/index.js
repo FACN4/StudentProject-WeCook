0dd659c1531784c80246e878c0efb7244033e67c
DenisKent <kentdenis6@hotmail.co.uk>
2018-09-10 23:37:46 +0300

    Setup back-end production capability for react router > #12

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,8 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/*', (req, res) => {
+router.get('/api', (req, res) => {
+  // eslint-disable-next-line no-console
   console.log(req.url);
-  res.send('Hello world');
+  res.send('Hello world!');
 });
+
 module.exports = router;
