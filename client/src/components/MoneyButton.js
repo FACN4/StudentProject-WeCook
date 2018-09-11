e1cf33de955e5203396ad5636591719a63886ae3
Matt Hall <matthall8@gmail.com>
2018-09-11 17:52:09 +0300

    Changed to functional component

--- client/src/components/MoneyButton.js
+++ client/src/components/MoneyButton.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
-import addToBasket from '../actions/addToBasket'
+import addToBasket from "../actions/addToBasket";
 
-class MoneyButton extends Component {
-  render() {
-    return (
-      <button onClick={this.props.addToBasket}>Add to basket</button>
-    );
-  }
-}
+const MoneyButton = props => (
+  <button onClick={props.addToBasket}>Add to basket</button>
+);
 
 MoneyButton.propTypes = {
   addToBasket: PropTypes.func.isRequired
-}
+};
 
-export default connect(null, {addToBasket})(MoneyButton);
+export default connect(
+  null,
+  { addToBasket }
+)(MoneyButton);
