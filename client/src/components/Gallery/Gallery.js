2128f7652556ea3f9913e55e203a84aaf385ed8a
DenisKent <kentdenis6@hotmail.co.uk>
2018-09-12 17:02:00 +0300

    Gallery component css finished > #22

--- client/src/components/Gallery/Gallery.js
+++ client/src/components/Gallery/Gallery.js
@@ -6,9 +6,9 @@ export default class Gallery extends Component {
   render() {
     return (
       <GalleryContainer>
-        <MainImg src='https://user-images.githubusercontent.com/28222381/45268801-1163b100-b48b-11e8-85b0-d32b09a111e3.jpg' alt="main image"/>
-        <Img1 src='https://user-images.githubusercontent.com/28222381/45268805-1c1e4600-b48b-11e8-932f-ddae68a9bd3c.jpg' alt="alternate image 1"/>
-        <Img2 src='https://user-images.githubusercontent.com/28222381/45268810-37895100-b48b-11e8-8b5d-c6e8964b4f2b.jpg' alt="alternate image 2"/>
+        <MainImg src='https://user-images.githubusercontent.com/28222381/45268814-4ff96b80-b48b-11e8-88ba-f9e7fa57dc22.jpg' alt="main image"/>
+        <Img1 src='https://user-images.githubusercontent.com/28222381/45268814-4ff96b80-b48b-11e8-88ba-f9e7fa57dc22.jpg' alt="alternate image 1"/>
+        <Img2 src='https://user-images.githubusercontent.com/28222381/45268814-4ff96b80-b48b-11e8-88ba-f9e7fa57dc22.jpg' alt="alternate image 2"/>
         <Img3 src='https://user-images.githubusercontent.com/28222381/45268814-4ff96b80-b48b-11e8-88ba-f9e7fa57dc22.jpg' alt="alternate image 3"/>
       </GalleryContainer>
     );
