f2ee80ed8da93925a104af70a5c5f843e7a0386c
DenisKent <kentdenis6@hotmail.co.uk>
2018-09-13 10:15:07 +0300

    Changed mealPage in to a functional component

--- client/src/pages/MealPage/MealPage.js
+++ client/src/pages/MealPage/MealPage.js
@@ -3,18 +3,18 @@ import PropTypes from 'prop-types';
 import {Header, MoneyButton} from '../../components'
 import MealInfo from './sections/MealInfo/MealInfo'
 
-export default class MealPage extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Header />
-        <MealInfo mealId={this.props.match.params.mealId}/>
-        <MoneyButton />
-      </React.Fragment>
-    );
-  }
+const MealPage = (props) => {
+  return (
+    <React.Fragment>
+      <Header />
+      <MealInfo mealId={props.match.params.mealId}/>
+      <MoneyButton />
+    </React.Fragment>
+  );
 }
 
 MealPage.propTypes = {
   match: PropTypes.object,
 };
+
+export default MealPage;
