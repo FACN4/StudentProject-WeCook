7399778ab7a4cf56fcc91be814bab4a63af309fc
Matt Hall <matthall8@gmail.com>
2018-09-16 08:39:41 +0300

    Updated GetMeal info send 500 error

--- server/controllers/mealInfo.js
+++ server/controllers/mealInfo.js
@@ -4,5 +4,5 @@ exports.get = (req, res) => {
   const { mealId } = req.params;
   getMealInfo(mealId)
     .then(row => res.send(row))
-    .catch(() => res.send('Could not find meal'));
+    .catch(() => res.status(500).send('Could not find meal'));
 };
