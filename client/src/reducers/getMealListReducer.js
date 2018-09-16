da7f1745866443005df4ad2ca61d7c0442cd0b83
Matt Hall <matthall8@gmail.com>
2018-09-16 11:25:20 +0300

    Updated to add 500 and rearrange reducers file

--- client/src/reducers/getMealListReducer.js
+++ client/src/reducers/getMealListReducer.js
@@ -4,12 +4,12 @@ const initialState = {};
 
 export default function(state = initialState, action) {
   switch (action.type) {
-    case `${GET_MEAL_LIST}_FULFILLED`:
-      return { data: action.payload.data, isFulfilled: true };
     case `${GET_MEAL_LIST}_PENDING`:
       return { isPending: true };
+    case `${GET_MEAL_LIST}_FULFILLED`:
+      return { data: action.payload.data, isFulfilled: true, isPending: false };
     case `${GET_MEAL_LIST}_REJECTED`:
-      return { isRejected: true, error: action.payload };
+      return { isRejected: true, error: action.payload, isPending: false };
     default:
       return state;
   }
