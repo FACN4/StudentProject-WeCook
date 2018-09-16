fb8631b8ea1355a96159f6b64a93458767d5086f
DenisKent <kentdenis6@hotmail.co.uk>
2018-09-16 20:17:58 +0300

    fixed order of getMealReviewsReducer > #65

--- client/src/reducers/getMealReviewsReducer.js
+++ client/src/reducers/getMealReviewsReducer.js
@@ -4,10 +4,10 @@ const initialState = {};
 
 export default function (state = initialState,action) {
   switch(action.type) {
-    case `${GET_MEAL_REVIEWS}_FULFILLED`:
-      return { data: action.payload.data, isFulfilled: true };
     case `${GET_MEAL_REVIEWS}_PENDING`:
       return { isPending: true };
+    case `${GET_MEAL_REVIEWS}_FULFILLED`:
+      return { data: action.payload.data, isFulfilled: true };
     case `${GET_MEAL_REVIEWS}_REJECTED`:
       return { isRejected: true, error: action.payload };
     default:
