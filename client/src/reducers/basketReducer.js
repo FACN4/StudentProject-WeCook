c2b548351e00797130d872acd276957ab66c6b08
Matt Hall <matthall8@gmail.com>
2018-09-17 17:33:59 +0300

    Updated remove to work with new object #83

--- client/src/reducers/basketReducer.js
+++ client/src/reducers/basketReducer.js
@@ -3,8 +3,7 @@ import { ADD_TO_BASKET, REMOVE_BASKET_ITEM } from "../actions/actionTypes";
 const initialState = {};
 
 export default function(state = initialState, action) {
-  let { basket: oldBasket } = state;
-  if (action.type == "ADD_TO_BASKET") {
+  if (action.type == ADD_TO_BASKET || action.type == REMOVE_BASKET_ITEM) {
     var { mealId, quantity, mealInfo } = action.payload;
     var acc = 0;
     if (state.hasOwnProperty(mealId)) {
@@ -17,14 +16,10 @@ export default function(state = initialState, action) {
         ...state,
         [mealId]: { quantity: quantity + acc, mealInfo }
       };
-    case REMOVE_BASKET_ITEM:
-      return {
-        ...state,
-        basket: [
-          ...oldBasket.slice(0, action.payload.mealId),
-          ...oldBasket.slice(action.payload.mealId + 1)
-        ]
-      };
+    case REMOVE_BASKET_ITEM: {
+      const { [mealId]: value, ...stateWithoutMeal } = state;
+      return stateWithoutMeal;
+    }
     default:
       return state;
   }
