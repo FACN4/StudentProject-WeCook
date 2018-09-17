4e153f89f546a902dbd8562b5a0139269196e1fb
Suha Afifi <39088518+afifisuha@users.noreply.github.com>
2018-09-17 12:01:18 +0300

    Update actionTypes.js

--- client/src/actions/actionTypes.js
+++ client/src/actions/actionTypes.js
@@ -1,8 +1,5 @@
 export const GET_MEAL_INFO = "GET_MEAL_INFO";
 export const ADD_TO_BASKET = "ADD_TO_BASKET";
-<<<<<<< HEAD
 export const GET_COOK_MEAL_INFO = "GET_COOK_MEAL_INFO";
-=======
 export const GET_MEAL_REVIEWS = "GET_MEAL_REVIEWS";
->>>>>>> 56e7dad0588ef2b78090c5b9b0158a7bccb13849
 export const GET_MEAL_LIST = "GET_MEAL_LIST";
