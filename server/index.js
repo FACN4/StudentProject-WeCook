3bf891fc2da5e41e263090d9a72057f308404fa0
Matt Hall <matthall8@gmail.com>
2018-09-18 11:44:37 +0300

    Updated server to use Express only

--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
-const http = require('http');
 const app = require('./app');
 
-http.createServer(app).listen(app.get('PORT'), () => {
-  // eslint-disable-next-line no-console
-  console.log('App running on port', app.get('PORT'));
+app.listen(app.get('port'), () => {
+  console.log('App running on port', app.get('port'));
 });
