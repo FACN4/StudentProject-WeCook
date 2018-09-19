98de57a1823814f2a958a143056fe7b428b82fee
Matt Hall <matthall8@gmail.com>
2018-09-19 15:29:12 +0300

    Updated name in DB fies

--- server/database/getQueries/getMealList.js
+++ server/database/getQueries/getMealList.js
@@ -3,7 +3,7 @@ const { db } = require('../db_connection');
 const getMealList = () => {
   const queryString = `
   SELECT
-    meal.id, meal.meal_title, meal.price, meal.final_booking_at, meal.remaining_portions, meal_thumbnails.meal_image_url, cook_info.cook_firstname, cook_thumbnails.cook_image_url, av_review.av_star_rating, av_review.count_reviews
+    meal.id, meal.meal_title, meal.price, meal.final_booking_at, meal.remaining_portions, meal_thumbnails.meal_image_url, cook_info.cook_firstname, cook_thumbnails.cook_image_url, av_review.avg_star_rating, av_review.count_reviews
   FROM
     (SELECT * FROM meals) AS meal
   LEFT JOIN
