3f81e407d7c8fb3a14f4ba3e49f815c0e69e1398
DenisKent <kentdenis6@hotmail.co.uk>
2018-09-20 00:18:17 +0300

    Checkout will go to login page if the user is not logged in > #127

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { connect } from "react-redux";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { hot } from "react-hot-loader";
 import {
   MealPage,
@@ -17,12 +18,16 @@ class Router extends Component {
       <BrowserRouter>
         <React.Fragment>
           <Switch>
+
             <Route exact path="/" component={HomePage} />
             <Route exact path="/mealList" component={MealsListPage} />
             <Route exact path="/mealInfo/:mealId" component={MealPage} />
             <Route exact path="/basket" component={BasketPage} />
             <Route exact path="/login" component={LoginPage} />
             <Route exact path="/register" component={RegisterPage} />
+            <Route exact path="/checkout"
+              render={() => (this.props.loggedIn ? (<CheckoutPage />) : (<Redirect to="/login"/>))}
+            />
             <Route exact path="/checkout" component={CheckoutPage} />
           </Switch>
         </React.Fragment>
@@ -31,4 +36,11 @@ class Router extends Component {
   }
 }
 
-export default hot(module)(Router);
+const mapStateToProps = ({userStatus:{loggedIn}}) => ({loggedIn:loggedIn})
+
+const connectedRouter = connect(
+  mapStateToProps,
+  null
+)(Router);
+
+export default hot(module)(connectedRouter);
