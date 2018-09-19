356e915aae33dab2ba54a7cfb1975711dbd666ef
DenisKent <kentdenis6@hotmail.co.uk>
2018-09-20 00:26:54 +0300

    redirect from login and register pages to basket page > #127

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import PropTypes from "prop-types";
 import { hot } from "react-hot-loader";
 import {
   MealPage,
@@ -23,18 +24,25 @@ class Router extends Component {
             <Route exact path="/mealList" component={MealsListPage} />
             <Route exact path="/mealInfo/:mealId" component={MealPage} />
             <Route exact path="/basket" component={BasketPage} />
-            <Route exact path="/login" component={LoginPage} />
-            <Route exact path="/register" component={RegisterPage} />
+            <Route exact path="/login"
+              render={() => (this.props.loggedIn ? (<Redirect to="/basket"/>):(<LoginPage />))}
+            />
+            <Route exact path="/register"
+              render={() => (this.props.loggedIn ? (<Redirect to="/basket"/>):(<RegisterPage />))}
+            />
             <Route exact path="/checkout"
               render={() => (this.props.loggedIn ? (<CheckoutPage />) : (<Redirect to="/login"/>))}
             />
-            <Route exact path="/checkout" component={CheckoutPage} />
           </Switch>
         </React.Fragment>
       </BrowserRouter>
     );
   }
 }
+
+Router.propTypes = {
+  loggedIn: PropTypes.bool,
+};
 
 const mapStateToProps = ({userStatus:{loggedIn}}) => ({loggedIn:loggedIn})
 
