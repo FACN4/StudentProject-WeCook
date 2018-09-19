27041e991851ce301ba93219e26197a79ef7bdc7
matthall8 <matthall8@gmail.com>
2018-09-19 07:10:45 +0300

    Removing unnecessary import

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,6 @@ import { Provider } from "react-redux";
 import { ThemeProvider } from "styled-components";
 
 import { store, persistor } from "./store";
-import { GlobalStyle } from "./App.style"; // eslint-disable-line no-unused-vars
 import wecookTheme from "./themes/weCookv1";
 import { Loading } from "./components";
 import "./assets/global.css";
