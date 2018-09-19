5f1e1a73815841ec1d9a5e2eafe82e95e68da4b8
Matt Hall <matthall8@gmail.com>
2018-09-19 08:14:09 +0300

    Solved render issue by installing webpack add on

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,6 @@ module.exports = {
     filename: 'index_bundle.js',
     publicPath: '/',
   },
-  target: 'node', // in order to ignore built-in modules like path, fs, etc.
-  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
   module: {
     rules: [
       {
